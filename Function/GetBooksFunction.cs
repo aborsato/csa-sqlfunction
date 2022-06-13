7806ef53b71833aa9316c193509a557b02cefb33
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2022-06-13 18:02:26 +0000

    get books function connection to SQL

--- Function/GetBooksFunction.cs
+++ Function/GetBooksFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,29 +8,22 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using csa.Model;
 
 namespace csa.Function
 {
     public static class GetBooksFunction
     {
         [FunctionName("GetBooksFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "books/{id}")] HttpRequest req,
+            ILogger log,
+            [Sql("SELECT Id, Author, Title, Pages FROM dbo.Book WHERE @Id = Id",
+                CommandType = System.Data.CommandType.Text,
+                Parameters = "@Id={id}",
+                ConnectionStringSetting = "SqlConnectionString")] IEnumerable<Book> books)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(books);
         }
     }
 }
---
+++ Model/Book.cs
@@ -0,0 +1,14 @@
+
+namespace csa.Model
+{
+    public class Book
+    {
+        public string Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public int Pages { get; set; }
+    }
+}
